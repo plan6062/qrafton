cc90dc2a5fbacb21c8e3ec6ead03024ac1198cf7
plan6062 <plan6062@gmail.com>
2025-05-13 00:08:58 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', title="Hello World")
+
+if __name__ == '__main__':
+    app.run(debug=True)
