29b82f46c067b125e3d04f86996dae548cb9f44d
ssumday24 <zx546800@naver.com>
2025-05-13 17:59:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,20 @@ client=MongoClient('localhost',27017)
 db = client.quiz  # 'quiz' 라는 DB
 #컬렉션은 (member,question,answer,rank 등 예정 )
 
+#필요한 함수선언 부분 ###################################
+def get_user_rank(userid):
+    members = list(db.member.find())
+    members.sort(key=lambda x: x.get('score', 0), reverse=True)
+    rank = next((i for i, m in enumerate(members, start=1) if m['userid'] == userid), None)
+    return members, rank
+
+
+
+
+
+##############################################
+
+
 # 첫 로그인 페이지 
 @app.route('/')
 def home():
@@ -73,20 +87,13 @@ def main():
         user = db.member.find_one({'userid': current_user_id})
 
         if user:
-            # 모든 멤버 가져와서 점수순 정렬
-            members = list(db.member.find())
-            members.sort(key=lambda x: x.get('score', 0), reverse=True)
-
-            # 로그인한 유저 순위 계산
-            rank_position = next(
-                (i for i, m in enumerate(members, start=1) if m['userid'] == current_user_id),
-                None
+            members, rank_position = get_user_rank(current_user_id)
+            return render_template(
+                'main.html',
+                nickname=user['nickname'],
+                members=members,
+                rank_position=rank_position
             )
-
-            return render_template('main.html',
-                                   nickname=user['nickname'],
-                                   members=members,
-                                   rank_position=rank_position)
     except jwt.ExpiredSignatureError:
         return redirect('/')
     except jwt.exceptions.DecodeError:
@@ -94,14 +101,48 @@ def main():
 
     return redirect('/')
 
+
 #퀴즈 시작 부분
 @app.route('/quiz/start')
-def start():
+def quiz_start():
     quiz_list=list(db.quiz_list.find())  # quiz 컬렉션
     quiz = random.choice(quiz_list)  # 랜덤으로 문제 하나 선택
     
     return render_template("quiz.html",quiz=quiz)
 
+#퀴즈 완료 부분
+@app.route('/quiz/finish')
+def quiz_finish():
+    token = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        current_user_id = payload['id']
+        user = db.member.find_one({'userid': current_user_id})
+
+        if user:
+            nickname = user['nickname']
+            correct_cnt = user.get('score', 0)  # 맞은 개수는 score 필드에서
+
+            # 모든 유저 점수 기준 정렬
+            members = list(db.member.find())
+            members.sort(key=lambda x: x.get('score', 0), reverse=True)
+
+            # 현재 유저 순위 계산
+            my_rank = next(
+                (i for i, m in enumerate(members, start=1) if m['userid'] == current_user_id),
+                None
+            )
+
+            return render_template("quiz_finish.html",
+                                   nickname=nickname,
+                                   correct_cnt=correct_cnt,
+                                   my_rank=my_rank)
+    except jwt.ExpiredSignatureError:
+        return redirect('/main')
+    except jwt.exceptions.DecodeError:
+        return redirect('/main')
+
+    return redirect('/main')
 
 
 
